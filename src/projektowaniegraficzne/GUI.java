54d942f1fb69bb9397a02b613bb67a2c8ef54882
Konrad Szczygieł‚ <kondiq.pl@gmail.com>
2016-01-27 21:11:36 +0100

    przerobiony przykład

--- src/projektowaniegraficzne/GUI.java
+++ src/projektowaniegraficzne/GUI.java
@@ -5,49 +5,345 @@
  */
 package projektowaniegraficzne;
 
-import javax.swing.JFrame;
+//import com.mxgraph.examples.swing.GraphEditor;
+//import com.mxgraph.examples.swing.GraphEditor.CustomGraphComponent;
+//import static com.mxgraph.examples.swing.GraphEditor.numberFormat;
+import com.mxgraph.examples.swing.editor.BasicGraphEditor;
+import com.mxgraph.examples.swing.editor.EditorMenuBar;
+import com.mxgraph.examples.swing.editor.EditorPalette;
+import com.mxgraph.io.mxCodec;
+import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxGeometry;
+import com.mxgraph.model.mxICell;
+import com.mxgraph.model.mxIGraphModel;
 
 import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.swing.util.mxSwingConstants;
+import com.mxgraph.util.mxConstants;
+import com.mxgraph.util.mxPoint;
+import com.mxgraph.util.mxResources;
+import com.mxgraph.util.mxUtils;
+import com.mxgraph.view.mxCellState;
 import com.mxgraph.view.mxGraph;
+import java.awt.Color;
+import java.awt.Point;
+import java.text.NumberFormat;
+import java.util.Iterator;
+import java.util.List;
+import javax.swing.ImageIcon;
+import javax.swing.UIManager;
+import org.w3c.dom.Document;
 
 
 /**
- * nie Główne okno programu. chyba do usunięcia
+ * Główne okno programu.
  * @author Kondiq
  */
-public class GUI extends JFrame{
+public class GUI extends BasicGraphEditor{
     
     /**
-    * nie Tworzy okno aplikacji.
+	 * Holds the shared number formatter.
+	 * 
+	 * @see NumberFormat#getInstance()
+	 */
+	public static final NumberFormat numberFormat = NumberFormat.getInstance();
+    
+    /**
+    * Tworzy okno aplikacji.
     */
     public GUI()
+    {
+            this("mxGraph Editor", new CustomGraphComponent(new CustomGraph()));
+    }
+    
+    /**
+     *
+     * @param appTitle
+     * @param component
+     */
+    public GUI(String appTitle, mxGraphComponent component)
+    {
+        super(appTitle, component);
+	final mxGraph graph = graphComponent.getGraph();
+        
+        /**
+	 * Tworzy palety ikon
+	 */
+        EditorPalette soundPalette = insertPalette(mxResources.get("sound"));
+        EditorPalette analizePalette = insertPalette(mxResources.get("analize"));
+        EditorPalette shapesPalette = insertPalette(mxResources.get("shapes"));
+        
+        soundPalette
+                                       .addTemplate(
+                                                       "Mic",
+                                                       new ImageIcon(
+                                                                       GUI.class
+                                                                                       .getResource("/projektowaniegraficzne/images/Mic.png")),
+                                                       "image;image=/projektowaniegraficzne/images/Mic.png",
+                                                       80, 50, "Mic");
+
+               soundPalette
+                                       .addTemplate(
+                                                       "SoundFile",
+                                                       new ImageIcon(
+                                                                       GUI.class
+                                                                                       .getResource("/projektowaniegraficzne/images/SoundFile.png")),
+                                                       "image;image=/projektowaniegraficzne/images/SoundFile.png",
+                                                       80, 50, "SoundFile");
+               analizePalette
+                                       .addTemplate(
+                                                       "FFT",
+                                                       new ImageIcon(
+                                                                       GUI.class
+                                                                                       .getResource("/projektowaniegraficzne/images/FFT.png")),
+                                                       "image;image=/projektowaniegraficzne/images/FFT.png",
+                                                       80, 50, "FFT");
+               
+               shapesPalette
+                                       .addTemplate(
+                                                       "Palette",
+                                                       new ImageIcon(
+                                                                       GUI.class
+                                                                                       .getResource("/projektowaniegraficzne/images/Palette.png")),
+                                                       "image;image=/projektowaniegraficzne/images/Palette.png",
+                                                       80, 50, "Palette");
+    }
+    
+    /**
+     * 
+     */
+    public static class CustomGraphComponent extends mxGraphComponent
+    {
+        /**
+		 * 
+		 * @param graph
+		 */
+		public CustomGraphComponent(mxGraph graph)
+		{
+			super(graph);
+
+			// Sets switches typically used in an editor
+			setPageVisible(true);
+			setGridVisible(true);
+			setToolTips(true);
+			getConnectionHandler().setCreateTarget(true);
+
+			// Loads the defalt stylesheet from an external file
+			mxCodec codec = new mxCodec();
+			Document doc = mxUtils.loadDocument(GUI.class.getResource(
+					//"/projektowaniegraficzne/resources/default-style.xml")
+                                        "/com/mxgraph/examples/swing/resources/default-style.xml")
+					.toString());
+			codec.decode(doc.getDocumentElement(), graph.getStylesheet());
+
+			// Sets the background to white
+			getViewport().setOpaque(true);
+			getViewport().setBackground(Color.WHITE);
+		}
+                
+                /**
+		 * Overrides drop behaviour to set the cell style if the target
+		 * is not a valid drop target and the cells are of the same
+		 * type (eg. both vertices or both edges). 
+             * @param cells
+             * @param dx
+             * @param dy
+             * @param target
+             * @param location
+             * @return 
+		 */
+                @Override
+		public Object[] importCells(Object[] cells, double dx, double dy,
+				Object target, Point location)
+		{
+			if (target == null && cells.length == 1 && location != null)
+			{
+				target = getCellAt(location.x, location.y);
+
+				if (target instanceof mxICell && cells[0] instanceof mxICell)
+				{
+					mxICell targetCell = (mxICell) target;
+					mxICell dropCell = (mxICell) cells[0];
+
+					if (targetCell.isVertex() == dropCell.isVertex()
+							|| targetCell.isEdge() == dropCell.isEdge())
+					{
+						mxIGraphModel model = graph.getModel();
+						model.setStyle(target, model.getStyle(cells[0]));
+						graph.setSelectionCell(target);
+
+						return null;
+					}
+				}
+			}
+
+			return super.importCells(cells, dx, dy, target, location);
+		}
+    }
+    
+    /**
+	 * A graph that creates new edges from a given template edge.
+	 */
+	public static class CustomGraph extends mxGraph
 	{
-		super("Projektowanie graficzne");
+		/**
+		 * Holds the edge to be used as a template for inserting new edges.
+		 */
+		protected Object edgeTemplate;
 
-		/*mxGraph graph = new mxGraph();
-		Object parent = graph.getDefaultParent();
-
-		graph.getModel().beginUpdate();
-		try
+		/**
+		 * Custom graph that defines the alternate edge style to be used when
+		 * the middle control point of edges is double clicked (flipped).
+		 */
+		public CustomGraph()
 		{
-			Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80,
-					30);
-			Object v2 = graph.insertVertex(parent, null, "World!", 240, 150,
-					80, 30);
-			graph.insertEdge(parent, null, "Edge", v1, v2);
-		}
-		finally
-		{
-			graph.getModel().endUpdate();
+			setAlternateEdgeStyle("edgeStyle=mxEdgeStyle.ElbowConnector;elbow=vertical");
 		}
 
-		mxGraphComponent graphComponent = new mxGraphComponent(graph);
-		getContentPane().add(graphComponent);*/
+		/**
+		 * Sets the edge template to be used to inserting edges.
+		 */
+		public void setEdgeTemplate(Object template)
+		{
+			edgeTemplate = template;
+		}
+
+		/**
+		 * Prints out some useful information about the cell in the tooltip.
+		 */
+		public String getToolTipForCell(Object cell)
+		{
+			String tip = "<html>";
+			mxGeometry geo = getModel().getGeometry(cell);
+			mxCellState state = getView().getState(cell);
+
+			if (getModel().isEdge(cell))
+			{
+				tip += "points={";
+
+				if (geo != null)
+				{
+					List<mxPoint> points = geo.getPoints();
+
+					if (points != null)
+					{
+						Iterator<mxPoint> it = points.iterator();
+
+						while (it.hasNext())
+						{
+							mxPoint point = it.next();
+							tip += "[x=" + numberFormat.format(point.getX())
+									+ ",y=" + numberFormat.format(point.getY())
+									+ "],";
+						}
+
+						tip = tip.substring(0, tip.length() - 1);
+					}
+				}
+
+				tip += "}<br>";
+				tip += "absPoints={";
+
+				if (state != null)
+				{
+
+					for (int i = 0; i < state.getAbsolutePointCount(); i++)
+					{
+						mxPoint point = state.getAbsolutePoint(i);
+						tip += "[x=" + numberFormat.format(point.getX())
+								+ ",y=" + numberFormat.format(point.getY())
+								+ "],";
+					}
+
+					tip = tip.substring(0, tip.length() - 1);
+				}
+
+				tip += "}";
+			}
+			else
+			{
+				tip += "geo=[";
+
+				if (geo != null)
+				{
+					tip += "x=" + numberFormat.format(geo.getX()) + ",y="
+							+ numberFormat.format(geo.getY()) + ",width="
+							+ numberFormat.format(geo.getWidth()) + ",height="
+							+ numberFormat.format(geo.getHeight());
+				}
+
+				tip += "]<br>";
+				tip += "state=[";
+
+				if (state != null)
+				{
+					tip += "x=" + numberFormat.format(state.getX()) + ",y="
+							+ numberFormat.format(state.getY()) + ",width="
+							+ numberFormat.format(state.getWidth())
+							+ ",height="
+							+ numberFormat.format(state.getHeight());
+				}
+
+				tip += "]";
+			}
+
+			mxPoint trans = getView().getTranslate();
+
+			tip += "<br>scale=" + numberFormat.format(getView().getScale())
+					+ ", translate=[x=" + numberFormat.format(trans.getX())
+					+ ",y=" + numberFormat.format(trans.getY()) + "]";
+			tip += "</html>";
+
+			return tip;
+		}
+
+		/**
+		 * Overrides the method to use the currently selected edge template for
+		 * new edges.
+		 * 
+		 * @param graph
+		 * @param parent
+		 * @param id
+		 * @param value
+		 * @param source
+		 * @param target
+		 * @param style
+		 * @return
+		 */
+		public Object createEdge(Object parent, String id, Object value,
+				Object source, Object target, String style)
+		{
+			if (edgeTemplate != null)
+			{
+				mxCell edge = (mxCell) cloneCells(new Object[] { edgeTemplate })[0];
+				edge.setId(id);
+
+				return edge;
+			}
+
+			return super.createEdge(parent, id, value, source, target, style);
+		}
+
 	}
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        GUI frame = new GUI();
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(1100, 700);
-	frame.setVisible(true);
+        try
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch (Exception e1)
+		{
+			e1.printStackTrace();
+		}
+
+		mxSwingConstants.SHADOW_COLOR = Color.LIGHT_GRAY;
+		mxConstants.W3C_SHADOWCOLOR = "#D3D3D3";
+
+		GUI editor = new GUI();
+		editor.createFrame(new EditorMenuBar(editor)).setVisible(true);
     }
 }
